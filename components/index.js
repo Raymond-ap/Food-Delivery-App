1ca49a48f34db2a82bb7556f79636148720799f1
Apungu Raymond <apunguray@gmail.com>
2022-08-27 01:07:06 +0800

    global exports

--- components/index.js
+++ components/index.js
@@ -1,9 +1,11 @@
 import FeaturedRow from './FeaturedRow'
-import HorizontalView from './Cards/HorizontalView'
+import HorizontalView from './Views/HorizontalView'
+import VerticalView from './Views/VerticalView'
 import Header from './Header'
 
 export {
     FeaturedRow,
     HorizontalView,
     Header,
+    VerticalView
 }
