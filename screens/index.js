d24331dc49e336ba0f40e68aeeffe0d309ba47ba
Apungu Raymond <apunguray@gmail.com>
2022-08-26 19:11:23 +0800

    setting up screens

---
+++ screens/index.js
@@ -0,0 +1,13 @@
+import Onboarding from "./Onboarding/Onboarding";
+import HomeScreen from "./Home/HomeScreen";
+import SearchScreen from "./Search/SearchScreen";
+import OrderScreen from "./Orders/OrderScreen";
+import ProfileScreen from "./Profile/ProfileScreen";
+
+export {
+    Onboarding,
+    HomeScreen,
+    OrderScreen,
+    SearchScreen,
+    ProfileScreen
+}
