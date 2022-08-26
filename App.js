fbbcda077baa6ea63fc1bdcca94da8d03540db9e
Apungu Raymond <apunguray@gmail.com>
2022-08-27 07:20:26 +0800

    set up screen options

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Onboarding } from './screens';
-import { TailwindProvider } from 'tailwindcss-react-native';
-import TabNavigator from './navigation/TabNavigator';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Onboarding, AddressScreen } from "./screens";
+import { TailwindProvider } from "tailwindcss-react-native";
+import TabNavigator from "./navigation/TabNavigator";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <TailwindProvider>
-      <Stack.Navigator initialRouteName='Onboarding' screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Onboarding" component={Onboarding} />
-        <Stack.Screen name="Home" component={TabNavigator} />
-      </Stack.Navigator>
+        <Stack.Navigator
+          initialRouteName="Onboarding"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Onboarding" component={Onboarding} />
+          <Stack.Screen name="Home" component={TabNavigator} />
+          <Stack.Screen
+            options={{
+              presentation: "modal",
+              animationTypeForReplace: "push",
+            }}
+            name="SetAddress"
+            component={AddressScreen}
+          />
+        </Stack.Navigator>
       </TailwindProvider>
     </NavigationContainer>
   );
 }
-
