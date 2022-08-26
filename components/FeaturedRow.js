db32b573bf1c002e823404a27e0839eb0bf04f20
Apungu Raymond <apunguray@gmail.com>
2022-08-26 20:57:29 +0800

    local update

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,23 +1,27 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import HorizontalView from "./Cards/HorizontalView";
 
-const FeaturedRow = ({ label, disabled, data }) => {
+const FeaturedRow = ({ label, disabled, render }) => {
   return (
-    <View className="my-3">
+    <View className="my-2">
       <TouchableOpacity
         disabled={disabled}
         activeOpacity={0.8}
         className="flex-row items-center justify-between mx-4"
       >
-        <Text className="capitalize text-base font-bold tracking-wider">
+        <Text className="capitalize text-lg font-bold tracking-wider">
           {label}
         </Text>
         {!disabled && (
           <View className="px-3 rounded-lg py-2 bg-green-200">
-            <Text className="text-sm capitalize font-bold tracking-wide text-gray-900">all</Text>
+            <Text className="text-sm capitalize font-bold tracking-wide text-gray-900">
+              all
+            </Text>
           </View>
         )}
       </TouchableOpacity>
+      <View className="pt-2">{render}</View>
     </View>
   );
 };
