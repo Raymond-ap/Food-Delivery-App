0e27c4efb0cfbe08a451475e691661de81defa8e
Apungu Raymond <apunguray@gmail.com>
2022-08-27 07:20:46 +0800

    handle onpress

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,28 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
 import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 const Header = () => {
-    return (
-      <View className="bg-white w-full py-3 px-4 flex-row justify-between items-center">
-        <TouchableOpacity activeOpacity={0.9} className="flex-1 flex-row items-center">
-          <Ionicons name="location-outline" size={18} color="#f87171" />
-          <Text className="capitalize text-sm px-1">select location here</Text>
-        </TouchableOpacity>
-        <TouchableOpacity activeOpacity={0.8}>
-          <Ionicons name="cart-outline" size={24} color="black" />
-        </TouchableOpacity>
-      </View>
-    );
-  };
+  const navigation = useNavigation();
+  return (
+    <View className="bg-white w-full py-3 px-4 flex-row justify-between items-center">
+      <TouchableOpacity
+        onPress={() => navigation.navigate("SetAddress")}
+        activeOpacity={0.9}
+        className="flex-1 flex-row items-center"
+      >
+        <Ionicons name="location-outline" size={18} color="#f87171" />
+        <Text className="capitalize text-sm px-1 font-bold">
+          select location here
+        </Text>
+        <Ionicons name="chevron-down" size={18} color="#f87171" />
+      </TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.8}>
+        <Ionicons name="cart-outline" size={24} color="black" />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-export default Header
+export default Header;
