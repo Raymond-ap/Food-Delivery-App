06c3922632e5bc281cf76f1bed4d59d7a5aa791c
Apungu Raymond <apunguray@gmail.com>
2022-08-29 17:28:35 +0800

    prepping component

--- screens/Search/SearchScreen.js
+++ screens/Search/SearchScreen.js
@@ -1,20 +1,161 @@
-import { View, Text, SafeAreaView, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Ionicons } from "@expo/vector-icons";
 
+const DUMMY_CATEGORY = [
+  {
+    name: "lauch",
+  },
+  {
+    name: "casual",
+  },
+  {
+    name: "daily menu",
+  },
+  {
+    name: "fast food",
+  },
+  {
+    name: "breakfast",
+  },
+  {
+    icon: "pizza-outline",
+    name: "pizza",
+  },
+  {
+    name: "brunch",
+  },
+  {
+    name: "dessert",
+  },
+  {
+    name: "dinner",
+  },
+  {
+    name: "burger",
+  },
+  {
+    name: "chinese",
+  },
+  {
+    name: "italian",
+  },
+  {
+    name: "shawarma",
+  },
+];
+
 const SearchScreen = () => {
+  const [showSearchScreen, setShowSearchScreen] = React.useState(false);
+  const [searchText, setSearchText] = React.useState("");
+
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <Header />
+      {showSearchScreen ? (
+        <RenderSearchScreen
+          setShowSearchScreen={setShowSearchScreen}
+          searchText={searchText}
+          setSearchText={setSearchText}
+        />
+      ) : (
+        <InitialScreen
+          setShowSearchScreen={setShowSearchScreen}
+          searchText={searchText}
+          setSearchText={setSearchText}
+        />
+      )}
       <StatusBar style="dark" translucent={false} backgroundColor={"#fff"} />
     </SafeAreaView>
   );
 };
 
-const Header = () => {
+const RenderSearchScreen = ({
+  setShowSearchScreen,
+  setSearchText,
+  searchText,
+}) => {
   return (
-    <View className="mx-4 py-2 bg-white">
+    <View className="flex-1">
+      <View
+        style={{
+          elevation: 5,
+        }}
+        className="px-4 pt-2 bg-white shadow-black flex-row items-center"
+      >
+        <TouchableOpacity
+          activeOpacity={0.9}
+          onPress={() => setShowSearchScreen(false)}
+        >
+          <Ionicons name="arrow-back" size={20} color="#4b5563" />
+        </TouchableOpacity>
+        <TextInput
+          className="mx-1 flex-1 px-4 py-2 text-base"
+          placeholder="Restaurants, food, or cuisine"
+          autoFocus
+          onChangeText={(text) => setSearchText(text)}
+          value={searchText}
+        />
+      </View>
+    </View>
+  );
+};
+
+const InitialScreen = ({ setShowSearchScreen, setSearchText }) => {
+  return (
+    <View className="flex-1 bg-white">
+      <Header setShowSearchScreen={setShowSearchScreen} />
+      <FlatList
+        data={DUMMY_CATEGORY}
+        ListHeaderComponent={
+          <View className="px-4 py-2">
+            <Text className="text-lg font-bold capitalize tracking-widest text-black">
+              popular categories
+            </Text>
+          </View>
+        }
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({ item }) => <CategoryItem item={item} setSearchText={setSearchText} setShowSearchScreen={setShowSearchScreen} />}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+const CategoryItem = ({ item, setShowSearchScreen, setSearchText }) => {
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        setSearchText(item.name);
+        setShowSearchScreen(true);
+      }}
+      activeOpacity={0.9}
+      className="px-4  my-1 py-3 flex-row items-center"
+    >
+      {item.icon && (
+        <View className="mr-2">
+          <Ionicons
+            name={item.icon || "fast-food-outline"}
+            size={20}
+            color="#f87171"
+          />
+        </View>
+      )}
+      <Text className="text-base capitalize text-black">{item?.name}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const Header = ({ setShowSearchScreen }) => {
+  return (
+    <View className="px-4 pt-2 bg-white">
       <Text className="text-xl font-bold capitalize tracking-wider text-black">
         search
       </Text>
@@ -22,7 +163,8 @@ const Header = () => {
         <Ionicons name="search" size={20} color="#ccc" />
         <TextInput
           className="flex-1 px-4 py-2 text-base"
-          placeholder="Restaurants, food, or cuisine "
+          placeholder="Restaurants, food, or cuisine"
+          onPressIn={() => setShowSearchScreen(true)}
         />
       </View>
     </View>
