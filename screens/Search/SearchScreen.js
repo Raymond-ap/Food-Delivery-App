e13c18772676e14b82000d2a3ca9f60b9fbac13b
Apungu Raymond <apunguray@gmail.com>
2022-08-30 07:58:14 +0800

    pull categories from list of restaurants and implement pull don to refresh

--- screens/Search/SearchScreen.js
+++ screens/Search/SearchScreen.js
@@ -5,57 +5,92 @@ import {
   TextInput,
   TouchableOpacity,
   FlatList,
+  Alert,
+  ActivityIndicator,
+  RefreshControl,
 } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Ionicons } from "@expo/vector-icons";
+import { db } from "../../firebase";
 
-const DUMMY_CATEGORY = [
-  {
-    name: "lauch",
-  },
-  {
-    name: "casual",
-  },
-  {
-    name: "daily menu",
-  },
-  {
-    name: "fast food",
-  },
-  {
-    name: "breakfast",
-  },
-  {
-    icon: "pizza-outline",
-    name: "pizza",
-  },
-  {
-    name: "brunch",
-  },
-  {
-    name: "dessert",
-  },
-  {
-    name: "dinner",
-  },
-  {
-    name: "burger",
-  },
-  {
-    name: "chinese",
-  },
-  {
-    name: "italian",
-  },
-  {
-    name: "shawarma",
-  },
-];
+const wait = (timeout) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+};
 
 const SearchScreen = () => {
   const [showSearchScreen, setShowSearchScreen] = React.useState(false);
   const [searchText, setSearchText] = React.useState("");
+  const [searchResult, setSearchResult] = React.useState([]);
+  const [categories, setCategories] = React.useState([]);
+  const [refreshing, setRefreshing] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+
+  // pull categories from list of restaurants
+  const fetchCategories = async () => {
+    try {
+      setLoading(true);
+      const restaurant = await db
+        .collection("restaurant")
+        .get()
+        .then((querySnapshot) => {
+          const data = querySnapshot.docs.map((doc) => {
+            return doc.data();
+          });
+          return data;
+        });
+      let categoriesArray = [];
+      restaurant.forEach((item) => {
+        const category = item.categories;
+        categoriesArray.push(category);
+      });
+      // remove duplicate and return unique categories
+      const uniqueCategories = [...new Set(categoriesArray.flat())];
+      setCategories(uniqueCategories);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      Alert.alert("Error", error.message, [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Retry",
+          onPress: () => fetchCategories(),
+        },
+      ]);
+    }
+  };
+
+  // Refresh data
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    fetchCategories();
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
+
+  React.useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  if (loading) {
+    return (
+      <SafeAreaView className="flex-1 bg-white">
+        <View className="px-4 pt-2 bg-white">
+          <Text className="text-xl font-bold capitalize tracking-wider text-black">
+            search
+          </Text>
+        </View>
+        <View className="flex-1 justify-center items-center">
+          <ActivityIndicator size="large" color="#000" />
+        </View>
+        <StatusBar style="dark" translucent={false} backgroundColor={"#fff"} />
+      </SafeAreaView>
+    );
+  }
 
   return (
     <SafeAreaView className="flex-1 bg-white">
@@ -70,6 +105,9 @@ const SearchScreen = () => {
           setShowSearchScreen={setShowSearchScreen}
           searchText={searchText}
           setSearchText={setSearchText}
+          categories={categories}
+          onRefresh={onRefresh}
+          refreshing={refreshing}
         />
       )}
       <StatusBar style="dark" translucent={false} backgroundColor={"#fff"} />
@@ -108,12 +146,21 @@ const RenderSearchScreen = ({
   );
 };
 
-const InitialScreen = ({ setShowSearchScreen, setSearchText }) => {
+const InitialScreen = ({
+  setShowSearchScreen,
+  setSearchText,
+  categories,
+  refreshing,
+  onRefresh,
+}) => {
   return (
     <View className="flex-1 bg-white">
       <Header setShowSearchScreen={setShowSearchScreen} />
       <FlatList
-        data={DUMMY_CATEGORY}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+        data={categories}
         ListHeaderComponent={
           <View className="px-4 py-2">
             <Text className="text-lg font-bold capitalize tracking-widest text-black">
@@ -122,7 +169,13 @@ const InitialScreen = ({ setShowSearchScreen, setSearchText }) => {
           </View>
         }
         keyExtractor={(_, index) => index.toString()}
-        renderItem={({ item }) => <CategoryItem item={item} setSearchText={setSearchText} setShowSearchScreen={setShowSearchScreen} />}
+        renderItem={({ item }) => (
+          <CategoryItem
+            item={item}
+            setSearchText={setSearchText}
+            setShowSearchScreen={setShowSearchScreen}
+          />
+        )}
         showsVerticalScrollIndicator={false}
       />
     </View>
@@ -133,22 +186,13 @@ const CategoryItem = ({ item, setShowSearchScreen, setSearchText }) => {
   return (
     <TouchableOpacity
       onPress={() => {
-        setSearchText(item.name);
+        setSearchText(item);
         setShowSearchScreen(true);
       }}
       activeOpacity={0.9}
       className="px-4  my-1 py-3 flex-row items-center"
     >
-      {item.icon && (
-        <View className="mr-2">
-          <Ionicons
-            name={item.icon || "fast-food-outline"}
-            size={20}
-            color="#f87171"
-          />
-        </View>
-      )}
-      <Text className="text-base capitalize text-black">{item?.name}</Text>
+      <Text className="text-base capitalize text-black">{item}</Text>
     </TouchableOpacity>
   );
 };
