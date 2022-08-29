ac41ca18fab475c3e0eb944e78c67e1859e55a1b
Apungu Raymond <apunguray@gmail.com>
2022-08-29 16:50:38 +0800

    render screen header

--- screens/Search/SearchScreen.js
+++ screens/Search/SearchScreen.js
@@ -1,7 +1,7 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TextInput } from "react-native";
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { Header } from '../../components'
+import { Ionicons } from "@expo/vector-icons";
 
 const SearchScreen = () => {
   return (
@@ -9,7 +9,24 @@ const SearchScreen = () => {
       <Header />
       <StatusBar style="dark" translucent={false} backgroundColor={"#fff"} />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SearchScreen
+const Header = () => {
+  return (
+    <View className="mx-4 py-2 bg-white">
+      <Text className="text-xl font-bold capitalize tracking-wider text-black">
+        search
+      </Text>
+      <View className="flex-row items-center mt-4 px-2 bg-[#f3f4f6] rounded-md">
+        <Ionicons name="search" size={20} color="#ccc" />
+        <TextInput
+          className="flex-1 px-4 py-2 text-base"
+          placeholder="Restaurants, food, or cuisine "
+        />
+      </View>
+    </View>
+  );
+};
+
+export default SearchScreen;
