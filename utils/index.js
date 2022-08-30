9267c6690e2e23bcdf8190221dad2ea1703aa1b1
Apungu Raymond <apunguray@gmail.com>
2022-08-31 03:52:20 +0800

    utils functions

---
+++ utils/index.js
@@ -0,0 +1,10 @@
+// Truncate the description to a certain length
+export const truncate = (str, n) => {
+  return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+};
+
+export const calculateDiscountPrice = (amount, percentage) => {
+  console.log(amount, percentage);
+  const discount = (amount / 100) * (100 - percentage);
+  return discount.toFixed(2);
+};
