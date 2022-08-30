78c6ac1ca81bdc3f0e80f7ea43446b073e299088
Apungu Raymond <apunguray@gmail.com>
2022-08-30 08:09:50 +0800

    global exports

--- screens/index.js
+++ screens/index.js
@@ -4,6 +4,8 @@ import SearchScreen from "./Search/SearchScreen";
 import OrderScreen from "./Orders/OrderScreen";
 import ProfileScreen from "./Profile/ProfileScreen";
 import AddressScreen from './Location/AddressScreen'
+import RestaurantDetail from "./Details/RestaurantDetail";
+import RestaurantInfo from "./Details/RestaurantInfo";
 
 export {
     Onboarding,
@@ -11,5 +13,7 @@ export {
     OrderScreen,
     SearchScreen,
     ProfileScreen,
-    AddressScreen
+    AddressScreen,
+    RestaurantDetail,
+    RestaurantInfo
 }
