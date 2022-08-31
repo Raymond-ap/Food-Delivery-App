ae601a854c18d02144b645174969913c618d29fc
Apungu Raymond <apunguray@gmail.com>
2022-09-01 02:57:08 +0800

    redux implementation

--- redux/slice/BasketSlice.js
+++ redux/slice/BasketSlice.js
@@ -9,9 +9,20 @@ export const BasketSlice = createSlice({
   initialState,
   reducers: {
     addToBasket: (state, action) => {
-      state.items = [...state, action.payload];
+      state.items = [...state.items, action.payload];
     },
-    removeFromBasket: (state, action) => {},
+    removeFromBasket: (state, action) => {
+      const index = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      let copy = [...state.items];
+      if (index !== -1) {
+        copy.splice(index, 1);
+      } else {
+        alert("Item not found");
+      }
+      state.items = copy;
+    },
   },
 });
 
@@ -19,5 +30,11 @@ export const BasketSlice = createSlice({
 export const { addToBasket, removeFromBasket } = BasketSlice.actions;
 
 export const selectBasketItems = (state) => state.basket.items;
+
+export const selectBasketItemsById = (state, id) =>
+  state.basket.items.filter((item) => item.id === id);
+
+export const selectBasketTotal = (state) =>
+  state.basket.items.reduce((total, item) => total += item.price, 0);
 
 export default BasketSlice.reducer;
