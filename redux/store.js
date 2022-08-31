ed50c8c3c284ff7b08a2dcf92a0675bbed6887fe
Apungu Raymond <apunguray@gmail.com>
2022-09-01 07:23:54 +0800

    add restaurant to store

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import basketReducer from './slice/BasketSlice'
+import restaurantReducer from './slice/RestaurantSlice'
 
 export const store = configureStore({
   reducer: {
-    basket: basketReducer
+    basket: basketReducer,
+    restaurant: restaurantReducer
   },
 })
