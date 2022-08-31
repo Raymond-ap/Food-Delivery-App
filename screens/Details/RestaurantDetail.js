ff324a90eb0c60bb6a0d684554394308b80de53e
Apungu Raymond <apunguray@gmail.com>
2022-09-01 01:01:09 +0800

    clean up

--- screens/Details/RestaurantDetail.js
+++ screens/Details/RestaurantDetail.js
@@ -26,6 +26,7 @@ import Animated from "react-native-reanimated";
 import BottomSheet from "reanimated-bottom-sheet";
 
 import { truncate, calculateDiscountPrice } from "../../utils";
+import { ErrorCard, Headline, MenuItem } from "../../components";
 
 const RestaurantDetail = ({ route }) => {
   const [isLoading, setIsLoading] = useState(true);
@@ -88,127 +89,6 @@ const RestaurantDetail = ({ route }) => {
   );
 };
 
-const MenuItem = ({ menuItem, percentage, discount }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <TouchableOpacity
-      onPress={() => setIsOpen(!isOpen)}
-      activeOpacity={0.9}
-      className="py-3 my-1 border-t border-gray-300"
-    >
-      <View className="flex-row">
-        <View className="flex-1">
-          <Text className="text-base font-bold capitalize tracking-wider">
-            {menuItem.name}
-          </Text>
-          <Text className="text-sm text-gray-600 tracking-wider">
-            {truncate(menuItem.description, 60)}
-          </Text>
-          <View className="py-2 flex-row">
-            {/* strike through text */}
-            <Text
-              className={`text-base text-gray-600 tracking-wider ${
-                discount && "line-through"
-              }`}
-            >
-              {`GH₵ ${parseInt(menuItem.price).toFixed(2)}`}
-            </Text>
-            {discount && (
-              <View className="ml-2 bg-red-700 px-2 rounded-full">
-                <Text className="text-base text-white tracking-wider">
-                  {`GH₵ ${calculateDiscountPrice(
-                    parseInt(menuItem.price),
-                    parseInt(percentage)
-                  )}`}
-                </Text>
-              </View>
-            )}
-          </View>
-        </View>
-        <Image
-          source={{ uri: menuItem.thumbnail }}
-          className="w-24 h-20 object-cover"
-        />
-      </View>
-      {isOpen && (
-        <View className=" mt-2 flex-row items-center">
-          <TouchableOpacity
-            activeOpacity={0.9}
-            className="bg-green-200 rounded-full h-8 w-10 items-center justify-center"
-          >
-            <Entypo name="minus" size={20} color="#000" />
-          </TouchableOpacity>
-          <Text className="text-base mx-3">3</Text>
-          <TouchableOpacity
-            activeOpacity={0.9}
-            className="bg-green-200 rounded-full h-8 w-10 items-center justify-center"
-          >
-            <Entypo name="plus" size={20} color="black" />
-          </TouchableOpacity>
-        </View>
-      )}
-    </TouchableOpacity>
-  );
-};
-
-const ErrorCard = () => {
-  return (
-    <View className="mx-2">
-      <View className="absolute bottom-5 h-12 items-center justify-center rounded-lg shadow-md bg-opacity-60 bg-gray-800 w-full">
-        <Text className="text-sm text-white tracking-wider font-bold">
-          Currently not accepting orders now
-        </Text>
-      </View>
-    </View>
-  );
-};
-
-const Headline = ({ item }) => {
-  const navigation = useNavigation();
-  return (
-    <View className="py-2 px-4 ">
-      <View className="flex-row justify-between">
-        <View className="flex-1">
-          <Text className="text-2xl capitalize font-bold tracking-wider">
-            {item?.name}
-          </Text>
-          <Text className="text-base text-gray-600 capitalize tracking-wider">{`delivery GH₵ ${item?.deliveryFee}.00`}</Text>
-        </View>
-        <View className="flex-row items-center">
-          <Ionicons name="star-sharp" size={15} color={"black"} />
-          <Text className="font-bold text-xl">{item?.rating}</Text>
-        </View>
-      </View>
-      {item?.discount && (
-        <View className=" flex-row items-center py-3 border-b border-gray-300">
-          <Ionicons name="pricetag" size={20} color={"#dc2626"} />
-          <Text className="text-base text-gray-600 mx-3">
-            {`${item?.discountPercent}% off on the entire menu `}
-          </Text>
-        </View>
-      )}
-      <View className="py-3 border-b border-gray-300">
-        <TouchableOpacity
-          activeOpacity={1}
-          onPress={() => navigation.navigate("RestaurantInfo", { info: item })}
-          className="items-center flex-row justify-between"
-        >
-          <View className="flex-row items-center">
-            <Ionicons name="warning-outline" size={15} color={"#4b5563"} />
-            <Text className="text-base text-gray-600 mx-3">
-              Allerges and contact details
-            </Text>
-          </View>
-          <Ionicons
-            name="chevron-forward-outline"
-            size={15}
-            color={"#4b5563"}
-          />
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-};
 
 const RenderForeground = () => {
   const navigation = useNavigation();
