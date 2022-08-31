76870dfd06d7276cb72751a1cdde7137f0739c47
Apungu Raymond <apunguray@gmail.com>
2022-09-01 06:33:50 +0800

    render basket total

--- components/BasketCard.js
+++ components/BasketCard.js
@@ -2,15 +2,17 @@ import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectBasketTotal } from "../redux/slice/BasketSlice";
+import { useNavigation } from "@react-navigation/native";
 
-const BasketCard = ({ onPress }) => {
+const BasketCard = () => {
   const total = useSelector(selectBasketTotal);
+  const navigation = useNavigation();
 
   return (
     <View className="bg-white py-3 px-4">
       <TouchableOpacity
+        onPress={() => navigation.navigate("BasketScreen")}
         activeOpacity={0.9}
-        onPress={onPress}
         className="h-14 rounded-full w-full items-center justify-center bg-green-600"
       >
         <Text className="text-lg font-bold text-white tracking-wider">
