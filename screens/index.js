db41aca0a03200981436b82441b18cf9ca33b360
Apungu Raymond <apunguray@gmail.com>
2022-09-03 16:41:20 +0800

    local update

--- screens/index.js
+++ screens/index.js
@@ -3,19 +3,21 @@ import HomeScreen from "./Home/HomeScreen";
 import SearchScreen from "./Search/SearchScreen";
 import OrderScreen from "./Orders/OrderScreen";
 import ProfileScreen from "./Profile/ProfileScreen";
-import AddressScreen from './Location/AddressScreen'
+import AddressScreen from "./Location/AddressScreen";
 import RestaurantDetail from "./Details/RestaurantDetail";
 import RestaurantInfo from "./Details/RestaurantInfo";
-import BasketScreen from './Basket/BasketScreen'
+import BasketScreen from "./Basket/BasketScreen";
+import DeliveryScreen from "./Delivery/DeliveryScreen";
 
 export {
-    Onboarding,
-    HomeScreen,
-    OrderScreen,
-    SearchScreen,
-    ProfileScreen,
-    AddressScreen,
-    RestaurantDetail,
-    RestaurantInfo,
-    BasketScreen
-}
+  Onboarding,
+  HomeScreen,
+  OrderScreen,
+  SearchScreen,
+  ProfileScreen,
+  AddressScreen,
+  RestaurantDetail,
+  RestaurantInfo,
+  BasketScreen,
+  DeliveryScreen,
+};
